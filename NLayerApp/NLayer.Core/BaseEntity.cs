4c35ac04500d36137021106d2e8ac75fa742c0a9
Kaan Furkan Cakiroglu <kfc0737@gmail.com>
2023-03-08 23:07:31 +0300

    Entities created . Interfaces not yet

---
+++ NLayerApp/NLayer.Core/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core
+{
+    public abstract class BaseEntity
+    {
+
+        // abstract class : can't create new object at abstract class . 
+        public int Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+    }
+}
