bbb1ab4ec9535c4861a2faca92389ac982f475e4
Kaan Furkan Çakıroğlu <kfc0737@gmail.com>
2023-04-12 16:59:32 +0300

    Super Heroes get methods created

---
+++ SuperHeroWebApı/SuperHeroAPIDotNet7/Controllers/SuperHeroController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPIDotNet7.Models;
+
+namespace SuperHeroAPIDotNet7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private static List<SuperHero> heroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id= 1,
+                    Name = "Batman",
+                    FirstName = "Bruce",
+                    LastName = "Wayne",
+                    Place = "Gotham",
+                    Enemy = "Joker",
+                    PowerRate= 91
+                },
+                new SuperHero
+                {
+                    Id= 2,
+                    Name = "IronMan",
+                    FirstName = "Tony",
+                    LastName = "Stark",
+                    Place = "New York City",
+                    Enemy = "Thanos",
+                    PowerRate = 97
+                }
+            };
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeroesAsync()
+        {
+
+            return Ok(heroes);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHero>> GetByIdHeroAsync(int id)
+        {
+
+            foreach (var h in heroes)
+            {
+                if (h.Id == id)
+                    return Ok(h);
+            };
+            // var hero = heroes.Find(x => x.Id == id);
+            return NotFound("Sorry .This hero does not exist !");
+        }
+
+
+    }
+}
---
+++ SuperHeroWebApı/SuperHeroAPIDotNet7/Models/SuperHero.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualBasic;
+
+namespace SuperHeroAPIDotNet7.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Place { get; set; }
+        public string? Enemy { get; set; }
+        public int PowerRate { get; set; }
+    }
+}
